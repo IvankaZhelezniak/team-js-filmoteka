040d73df690439a183c8ec94f622ee7a6a3c01d4
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2022-07-19 17:56:47 +0300

    it remains to set the genres of JSit remains to set the genres of JS

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,4 +4,12 @@ const refs = {
   backdrop: document.querySelector('#backdrop'),
   body: document.querySelector('body'),
   btnCloseModalFilm: document.querySelector('#btn-close-modal'),
+  imageModal: document.querySelector('#imageModal'),
+  modalTitle: document.querySelector('#title'),
+  modalTitleOriginal: document.querySelector('#titleOriginal'),
+  voteModal: document.querySelector('#vote'),
+  votesModal: document.querySelector('#votes'),
+  popularityModal: document.querySelector('#popularity'),
+  genreModal: document.querySelector('#genre'),
+  overviewModal: document.querySelector('#overview'),
 };
