57395a5a237e5bf751c1ecf924dac912155dcdf2
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2022-07-20 18:46:57 +0300

    Фиксит жанры в портфолио

--- src/js/portfolio/movieClass.js
+++ src/js/portfolio/movieClass.js
@@ -106,19 +106,24 @@ export {
 	  return Object.values(genresArray).join(', ');
 	}
   
-	makeGenresList(genre_ids, genres) {
+	makeGenresList(genres_id, genres) {
 	  const genresArray = [];
-	  for (let id of genre_ids) {
-		if (genres[id] === null || genres[id] === undefined) {
+	  
+	  for (let id of genres_id) {
+		const index = genres.findIndex(genre => genre.id == id);
+		
+		if (genres[index] === null || genres[index] === undefined) {
 		  continue;
 		}
+
 		// если массив 2+ жанров -- пишем 'Other'
 		if (genresArray.length === 2) {
 		  genresArray.push('Other');
 		  break;
 		}
-		genresArray.push(genres[id].name);
-	  }
+		
+		genresArray.push(genres[index].name);
+	  }	  
 	  return Object.values(genresArray).join(', ');
 	}
   
