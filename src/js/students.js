c63b86ddb849a15ac88748930a88b1c7c730d6ca
IrynaBielova <wintergirl@i.ua>
2022-07-21 19:14:09 +0300

    team-cards

--- src/js/students.js
+++ src/js/students.js
@@ -1,14 +1,72 @@
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[data-modal-open]'),
+//       closeModalBtn: document.querySelector('[data-modal-close]'),
+//       modal: document.querySelector('[data-modal]'),
+//     };
+  
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+//     function toggleModal() {
+//       refs.modal.classList.toggle('is-hidden');
+//     }
+
+//     const onEscaPress = (evt) => {
+//       if (evt.code === "Escape") {
+//         modal.close();
+//       }};
+
+//       function onCloseModal() {
+//         refs.modal.classList.add('is-hidden');
+//         refs.modal.classList.remove('mount')
+//         document.removeEventListener('keydown', modalCloseEsc);
+//     };
+    
+//   })();
+
 (() => {
-    const refs = {
+  const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+      backSide: document.querySelectorAll('.students__card'),
+      backdrop: document.querySelector('.backdrop__students'),
+  };
+
+  refs.openModalBtn.addEventListener('click', onOpenModal);
+
+
+  refs.backSide.forEach((elem) => {
+      elem.addEventListener('click', toggleBackSide);
+      function toggleBackSide() {
+          elem.classList.toggle('active');
+      }
+  })
+
+  function onOpenModal(e) {
+      e.preventDefault()
+      refs.modal.classList.remove('is-hidden');
+      refs.modal.classList.add('mount');
+      refs.backdrop.addEventListener('click', modalCloseClickBackdrop);
+      document.addEventListener('keydown', modalCloseEsc);
+  }
+
+  function modalCloseClickBackdrop(e) {
+      if (e.target.nodeName === 'BACKDROP') {
+          onCloseModal()
+      }
+  }
+
+  function modalCloseEsc(e) {
+      if (e.code === 'Escape') {
+          onCloseModal()
+      }
+  }
+
+  function onCloseModal() {
+      refs.modal.classList.add('is-hidden');
+      refs.modal.classList.remove('mount')
+      document.removeEventListener('keydown', modalCloseEsc);
+  }
+})();
