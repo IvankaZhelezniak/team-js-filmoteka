7b424002c6d3e79d8e86f35e11c48a835f0cd417
Iryna Bielova <wintergirl@i.ua>
2022-07-24 12:25:19 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ import './js/loader';
 import './js/refs';
 import './js/auth/authenticationFirebase';
 import './js/portfolio/cteateGalleryStickers';
-// import './js/students';
+import './js/students';
