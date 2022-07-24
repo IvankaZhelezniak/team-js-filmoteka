86a2369b39a65c209182f4f8d924cfa511e06904
Ivanka Zhelezniak <ivankaz685@gmail.com>
2022-07-24 20:23:29 +0200

    add firebase

--- src/js/auth/authenticationFirebase.js
+++ src/js/auth/authenticationFirebase.js
@@ -20,6 +20,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const user = auth.currentUser;
+
+// if (user) {
+//   // User is signed in, see docs for a list of available properties
+//   // https://firebase.google.com/docs/reference/js/firebase.User
+//   // ...
+// } else {
+//   // No user is signed in.
+// }
+
 
 
 //listen for auth status changes
