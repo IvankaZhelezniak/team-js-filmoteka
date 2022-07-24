405b3a79f38a6526abe036cbf48e484acd97bc6c
IvankaZhelezniak <69460460+IvankaZhelezniak@users.noreply.github.com>
2022-07-24 20:26:43 +0000

    Deploying to gh-pages from @ IvankaZhelezniak/team-js-filmoteka@1d5bc680fb31570d5fbba86880d772f67902130e 🚀

---
+++ library.d800d40d.js
@@ -0,0 +1,2 @@
+var mask=document.querySelector(".mask");window.addEventListener("load",(function(){mask.classList.add("done"),setTimeout((function(){mask.remove()}),1e3)}));
+//# sourceMappingURL=library.d800d40d.js.map
