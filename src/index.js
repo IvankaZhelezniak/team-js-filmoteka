2becb0a23704bbe92ba9e608526723b05e2ac705
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-07-26 00:07:50 +0300

    fixed small bugs
    fixed console mistake on library page
    fixed hover on library`s buttons
    added margins-bottom for ul in library
    switched-on top button
    added autofocus for Warched button

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import './js/registerLoginForm';
 import './js/theme-toggle';
 import './js/portfolio/createStartPortfolio';
-// import './js/portfolio/watchedBtn';
-
 import './js/portfolio/movieClass';
-
 import './js/portfolio/modalFilm';
 import './js/portfolio/searchByName';
 import './js/portfolio/infinite-scroll-trending';
-
 import './js/modal/modalAddToLSWatchedQueue';
 import './js/portfolio/buttonTop';
 import './js/loader';
 import './js/refs';
 import './js/auth/authenticationFirebase';
 import './js/portfolio/cteateGalleryStickers';
-
 import './js/portfolio/genres';
-
 import './js/students';
