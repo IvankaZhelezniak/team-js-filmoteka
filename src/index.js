e27e0dcedb5b35254651d1ce0a44cc7cc3ab4c9f
Ivanka Zhelezniak <ivankaz685@gmail.com>
2022-07-25 17:49:02 +0200

    movie

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './js/registerLoginForm';
 import './js/theme-toggle';
 import './js/portfolio/createStartPortfolio';
 // import './js/portfolio/watchedBtn';
+
+import './js/portfolio/movieClass';
+
 import './js/portfolio/modalFilm';
 import './js/portfolio/searchByName';
 import './js/portfolio/infinite-scroll-trending';
@@ -12,5 +15,7 @@ import './js/loader';
 import './js/refs';
 import './js/auth/authenticationFirebase';
 import './js/portfolio/cteateGalleryStickers';
+
 import './js/portfolio/genres';
+
 import './js/students';
